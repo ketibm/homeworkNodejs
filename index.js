2b088386ed02218179375d6a41e181f5465a742a
ketibm <146774511+ketibm@users.noreply.github.com>
2024-02-12 20:54:36 +0100

    mongo/domashna3

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const express = require("express");
-const app = express();
 const {
-  getStudents,
-  redirectStudents,
-  getStudentData,
-  deleteStudent,
-} = require("./handlers/data");
+  getAllBooks,
+  getOneBook,
+  getSortedByTitle,
+  getBookByLanguage,
+  getBookByAuthor,
+  createBook,
+  updateBook,
+  removeBook,
+} = require("./controllers/book");
+require("./db/config");
+const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-app.get("/form", getStudents);
-app.post("/form", redirectStudents);
-app.get("/studenti", getStudentData);
-app.get("/brishi", deleteStudent);
 
-app.listen(3000, () => console.log("Server started at port 3000"));
+app.get("/", (req, res) => {
+  res.send("Hello from the server!");
+});
+
+app.get("/books", getAllBooks);
+app.get("/books/id/:id", getOneBook);
+app.get("/books/sorted", getSortedByTitle);
+app.get("/books/language/:language", getBookByLanguage);
+app.get("/books/author/:author", getBookByAuthor);
+app.post("/books", createBook);
+app.put("/books/:id", updateBook);
+app.delete("/books/:id", removeBook);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server started at port: ${port}`);
+});
